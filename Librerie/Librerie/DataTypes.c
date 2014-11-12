1df59917ad6662fdcc1735ce9e3c50ea43ee44c3
Alessandro Fusco <alessandro.fusco3@studio.unibo.it>
2014-11-12 15:52:47 +0100

    Riscrittura 1 da strutturare meglio

---
+++ Librerie/Librerie/DataTypes.c
@@ -0,0 +1,9 @@
+//
+//  DataTypes.c
+//  Librerie
+//
+//  Created by Alessandro Fusco on 12/11/14.
+//  Copyright (c) 2014 Alessandro Fusco. All rights reserved.
+//
+
+#include "DataTypes.h"
---
+++ Librerie/Librerie/DataTypes.h
@@ -0,0 +1,14 @@
+//
+//  DataTypes.h
+//  Librerie
+//
+//  Created by Alessandro Fusco on 12/11/14.
+//  Copyright (c) 2014 Alessandro Fusco. All rights reserved.
+//
+
+#ifndef __Librerie__DataTypes__
+#define __Librerie__DataTypes__
+
+#include <stdio.h>
+
+#endif /* defined(__Librerie__DataTypes__) */
