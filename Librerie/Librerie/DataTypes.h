c5c1ebbc0bc01c58175e5bd0994606d42c042e60
Alessandro Fusco <alessandro.fusco3@studio.unibo.it>
2014-11-12 21:55:35 +0100

    no message

--- Librerie/Librerie/DataTypes.h
+++ Librerie/Librerie/DataTypes.h
@@ -11,4 +11,25 @@
 
 #include <stdio.h>
 
+typedef enum {false, true} BOOL;
+typedef TYPE TYPE_A; //PER LA LIBRERIA ARRAY
+typedef TYPE TYPE_L; //PER LA LIBRERIA LIST
+
+//INPUT
+TYPE_A scanType();
+
+
+void printType(TYPE_A a);
+BOOL equals(TYPE_A a, TYPE_A b);
+BOOL isGreaterThan(TYPE_A a, TYPE_A b);
+
+
+BOOL isLessThan(TYPE_A a, TYPE_A b);
+BOOL isGreaterOrEqualThan(TYPE_A a, TYPE_A b);
+BOOL isLessOrEqualThan(TYPE_A a, TYPE_A b);
+int compare(TYPE_A a, TYPE_A b);
+void copy(TYPE_A *b, TYPE_A a);
+void copySorted(TYPE_A arr1[], int dim1, TYPE_A result[]);
+
+
 #endif /* defined(__Librerie__DataTypes__) */
